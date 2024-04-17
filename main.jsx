3af5c39a8b9cea766c7d335709127319d45e0a32
Md Ahadullah <108590177+ahad5333@users.noreply.github.com>
2024-04-17 23:56:13 +0530

    Counter App
    created a counter app using react

---
+++ main.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <h1>welcome to react</h1>
+    <App />
+  </>,
+)
